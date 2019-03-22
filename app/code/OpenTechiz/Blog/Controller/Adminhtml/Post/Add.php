f597aa6509dada47055e790c709f6cd73d8b4466
TrungPham2810 <trungphamvan2810@gmail.com>
2019-03-22 15:38:28 +0700

    action to add and edit blog

--- app/code/OpenTechiz/Blog/Controller/Adminhtml/Post/Add.php
+++ app/code/OpenTechiz/Blog/Controller/Adminhtml/Post/Add.php
@@ -11,8 +11,37 @@ namespace OpenTechiz\Blog\Controller\Adminhtml\Post;
 
 class Add extends \Magento\Backend\App\Action
 {
-    public function  execute()
+    /**
+     * @var \Magento\Backend\Model\View\Result\Forward
+     */
+    protected $resultForwardFactory;
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+    ) {
+        $this->resultForwardFactory = $resultForwardFactory;
+        parent::__construct($context);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
     {
-        // TODO: Implement execute() method.
+        return $this->_authorization->isAllowed('OpenTechiz_Blog::save');
+    }
+    /**
+     * Forward to edit
+     *
+     * @return \Magento\Backend\Model\View\Result\Forward
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Forward $resultForward */
+        $resultForward = $this->resultForwardFactory->create();
+        return $resultForward->forward('edit');
     }
 }
