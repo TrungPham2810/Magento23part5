9fdf2cbf31b20eeb52ba5857eeaef1a42c93cff5
TrungPham2810 <trungphamvan2810@gmail.com>
2019-03-22 21:07:50 +0700

    code to show blog in fontend

--- app/code/OpenTechiz/Blog/Block/PostView.php
+++ app/code/OpenTechiz/Blog/Block/PostView.php
@@ -4,41 +4,49 @@ class PostView extends \Magento\Framework\View\Element\Template implements
     \Magento\Framework\DataObject\IdentityInterface
 {
     protected $_commentCollectionFactory;
+    protected $_registry;
+
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \OpenTechiz\Blog\Model\Post $post,
         \OpenTechiz\Blog\Model\PostFactory $postFactory,
         \OpenTechiz\Blog\Model\ResourceModel\Comment\CollectionFactory $commentCollectionFactory,
+        \Magento\Framework\Registry $registry,
         array $data = []
     )
     {
+        $this->_registry = $registry;
         $this->_post = $post;
         $this->_commentCollectionFactory = $commentCollectionFactory;
         parent::__construct($context, $data);
         $this->_postFactory = $postFactory;
     }
 
-    public function _prepareLayout()
-    {
-        //set page title
-        $post = $this->getPost();
-
-        $this->pageConfig->getTitle()->set(__($post->getTitle()));
-
-        return parent::_prepareLayout();
-    }
+//    public function _prepareLayout()
+//    {
+//        //set page title
+//        $post = $this->getPost();
+//
+//        $this->pageConfig->getTitle()->set(__($post->getTitle()));
+//
+//        return parent::_prepareLayout();
+//    }
 
     public function getPost()
     {
-        if (!$this->hasData('posts')) {
-            if ($this->getPostId()) {
-                $post = $this->_postFactory->create();
-            } else {
-                $post = $this->_post;
-            }
-            $this->setData('posts', $post);
-        }
-        return $this->getData('posts');
+//        if (!$this->hasData('posts')) {
+//            if ($this->getPostId()) {
+//                $post = $this->_postFactory->create();
+//            } else {
+//                $post = $this->_post;
+//            }
+//            $this->setData('posts', $post);
+//        }
+        $post_id = $this->_registry->registry('post_id');
+
+//        return $this->getData('posts');
+        return $this->_postFactory->create()->load($post_id);
+
     }
 
     public function getIdentities()
--- app/code/OpenTechiz/Blog/Controller/View/Index.php
+++ app/code/OpenTechiz/Blog/Controller/View/Index.php
@@ -1,30 +1,46 @@
 <?php
 namespace OpenTechiz\Blog\Controller\View;
 use \Magento\Framework\App\Action\Action;
+use OpenTechiz\Blog\Model\PostFactory;
+
 class Index extends Action
 {
     protected $_postHelper;
+    protected $_postFactory;
+    protected $_registry;
+    protected $_pageFactory;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\Controller\Result\ForwardFactory $resultForwardFactory,
-        \OpenTechiz\Blog\Helper\Post $postHelper
+        \Magento\Framework\Registry $registry,
+        \OpenTechiz\Blog\Helper\Post $postHelper,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
     )
     {
         $this->_resultForwardFactory = $resultForwardFactory;
         $this->_postHelper = $postHelper;
+        $this->_registry = $registry;
+        $this->_pageFactory = $pageFactory;
         parent::__construct($context);
     }
 
     public function execute()
     {
-        $post_id = $this->getRequest()->getParam('post_id', $this->getRequest()->getParam('id', false));
-        echo $post_id;
+        $id = $this->_request->getParams();
+        $post_id = $id['id'];
+        $this->_registry->register('post_id', $post_id);
+//        echo $post_id;
+//        $post_id = $this->getRequest()->getParam('post_id', $this->getRequest()->getParam('id', false));
+//        echo $post_id;
+        //$post_id = $this->_postFactory->create()->getCollection()->getPostId();
+        //echo $post_id;
 //        $result_page = $this->_postHelper->prepareResultPost($this, $post_id);
 //        if (!$result_page) {
 //            $resultForward = $this->_resultForwardFactory->create();
 //            return $resultForward->forward('noroute');
 //        }
 //        return $result_page;
+        return $this->_pageFactory->create();
     }
 }
