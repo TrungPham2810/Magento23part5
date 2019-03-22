d466202b6d4ef23c4870e3618ac1dd129a258955
TrungPham2810 <trungphamvan2810@gmail.com>
2019-03-22 15:34:41 +0700

    code to view each blog

---
+++ app/code/OpenTechiz/Blog/Block/PostView.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trungpham
+ * Date: 22/03/2019
+ * Time: 14:17
+ */
+
+namespace OpenTechiz\Blog\Block;
+
+
+class PostView
+{
+
+}
---
+++ app/code/OpenTechiz/Blog/Controller/View/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trungpham
+ * Date: 22/03/2019
+ * Time: 14:31
+ */
+
+namespace OpenTechiz\Blog\Controller\View;
+
+
+class Index
+{
+
+}
