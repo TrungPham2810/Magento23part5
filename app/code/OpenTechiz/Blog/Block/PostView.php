3e48eb74c7b125cfd1a6f1d1b9bf38ae25d8015a
TrungPham2810 <trungphamvan2810@gmail.com>
2019-03-22 15:35:35 +0700

    code to view each blog

--- app/code/OpenTechiz/Blog/Block/PostView.php
+++ app/code/OpenTechiz/Blog/Block/PostView.php
@@ -1,15 +1,57 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: trungpham
- * Date: 22/03/2019
- * Time: 14:17
- */
-
 namespace OpenTechiz\Blog\Block;
-
-
-class PostView
+class PostView extends \Magento\Framework\View\Element\Template implements
+    \Magento\Framework\DataObject\IdentityInterface
 {
+    protected $_commentCollectionFactory;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \OpenTechiz\Blog\Model\Post $post,
+        \OpenTechiz\Blog\Model\PostFactory $postFactory,
+        \OpenTechiz\Blog\Model\ResourceModel\Comment\CollectionFactory $commentCollectionFactory,
+        array $data = []
+    )
+    {
+        $this->_post = $post;
+        $this->_commentCollectionFactory = $commentCollectionFactory;
+        parent::__construct($context, $data);
+        $this->_postFactory = $postFactory;
+    }
 
+    public function _prepareLayout()
+    {
+        //set page title
+        $post = $this->getPost();
+
+        $this->pageConfig->getTitle()->set(__($post->getTitle()));
+
+        return parent::_prepareLayout();
+    }
+
+    public function getPost()
+    {
+        if (!$this->hasData('posts')) {
+            if ($this->getPostId()) {
+                $post = $this->_postFactory->create();
+            } else {
+                $post = $this->_post;
+            }
+            $this->setData('posts', $post);
+        }
+        return $this->getData('posts');
+    }
+
+    public function getIdentities()
+    {
+        $identities = $this->getPost()->getIdentities();
+        $comments = $this->_commentCollectionFactory
+            ->create()
+            ->addFieldToFilter('comment_id', $this->getID())
+            ->addFieldToFilter('is_active', '1');
+        foreach ($comments as $comment) {
+            $identities = array_merge($identities,
+                [\OpenTechiz\Blog\Model\Comment::CACHE_COMMENT_POST_TAG."_".$comment->getID()]);
+        }
+        return ($identities);
+    }
 }
